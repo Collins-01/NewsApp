ed509c89ed7f24a2603b5e361382411a7f510271
Collins Oriakhi <collins.oriakhi@itara.co>
2022-04-05 16:21:01 +0100

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Oriakhi Collins on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
